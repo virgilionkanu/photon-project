918a77f4d52403be0f59f7f522d06ddcf1a693d8
Virgilio Cano <virgiliocano53@gmail.com>
2020-07-22 17:59:33 -0300

    Search clear

--- app.js
+++ app.js
@@ -32,8 +32,12 @@ function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-            <p>${photo.photographer}</p>
+    galleryImg.innerHTML = `
+            <div class="gallery-info">
+                <p>${photo.photographer}</p>
+                <a href="${photo.src.original}">Download</a>
+            </div>
+            <img src=${photo.src.large}></img>
           `;
     gallery.appendChild(galleryImg);
   });
@@ -47,10 +51,16 @@ async function curatedPhotos() {
 }
 
 async function searchPhotos(query) {
+  clear();
   const data = await fetchApi(
     `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`
   );
   generatePictures(data);
+}
+
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
 }
 
 curatedPhotos();
