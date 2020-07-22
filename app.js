1d8a827ac223c0d79354b3e7fa17ca6f193916c1
Virgilio Cano <virgiliocano53@gmail.com>
2020-07-22 17:47:33 -0300

    Search

--- app.js
+++ app.js
@@ -2,30 +2,55 @@ const auth = "563492ad6f917000010000017e30da49a8ea436c8c673c1ff7a8cfb1";
 
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-button");
-
+const form = document.querySelector(".search-form");
 let searchValue;
 
-async function curatedPhotos() {
-  const dataFetch = await fetch(
-    "https://api.pexels.com/v1/curated?per_page=15&page=1",
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: auth,
-      },
-    }
-  );
+//Event Listeners
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: auth,
+    },
+  });
   const data = await dataFetch.json();
+  return data;
+}
+
+function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
     galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-        <p>${photo.photographer}</p>
-      `;
+            <p>${photo.photographer}</p>
+          `;
     gallery.appendChild(galleryImg);
   });
+}
+
+async function curatedPhotos() {
+  const data = await fetchApi(
+    "https://api.pexels.com/v1/curated?per_page=15&page=1"
+  );
+  generatePictures(data);
+}
+
+async function searchPhotos(query) {
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`
+  );
+  generatePictures(data);
 }
 
 curatedPhotos();
